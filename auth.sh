17835be378bb265bc20ee7c20740171777d1283d
mrivadeneira <maxirivadeneira92@gmail.com>
2022-08-15 22:33:59 -0300

    first commit

---
+++ auth.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+ssh-keygen -t ed25519 -C "maxirivadeneira92@gmail.com"
+#Then: Settings > SSH and GPG keys > New SSH key
---
+++ push.sh
@@ -0,0 +1,7 @@
+#Execute the following after authentication is set up
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:mrivadeneira/<git repo>.git
+git push -u origin main
